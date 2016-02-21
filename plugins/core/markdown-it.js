fe18a44530689190f102838b51bc46b11e5e448b
Kurt Harriger <kharriger@rallydev.com>
2016-02-21 16:13:45 -0700

    add markdown-it-container for arbitrary classed divs

--- plugins/core/markdown-it.js
+++ plugins/core/markdown-it.js
@@ -44,6 +44,42 @@ md
   .use(require('markdown-it-deflist'))
   .use(require('markdown-it-ins'))
   .use(require('markdown-it-abbr'))
-  .use(require('markdown-it-checkbox'));
+  .use(require('markdown-it-checkbox'))
 
-exports.md = md 
+  .use(require('markdown-it-container'), 'spoiler', {
+
+    validate: function(params) {
+      return params.trim().match(/^spoiler\s+(.*)$/);
+    },
+
+    render: function (tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        return '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary>\n';
+
+      } else {
+        // closing tag
+        return '</details>\n';
+      }
+    }
+  })
+  .use(require('markdown-it-container'), '',
+   {
+    validate: function(params) {
+      return true;
+    },
+    render: function (tokens, idx) {
+      var classNames = tokens[idx].info.trim();
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        return '<div class=\'' + classNames + '\'>\n';
+      } else {
+        // closing tag
+        return '</div>\n';
+      }
+    }
+  });
+  
+exports.md = md
